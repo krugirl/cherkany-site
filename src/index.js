fd59fb0ddc545d057cd68ed9dedc332f255a6e57
kruboy <starkkruboy@gmail.com>
2021-02-11 22:20:44 +0200

    add cherkanyButton block

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
 import "./scss/index.scss";
 
 
-// const header = document.querySelector(".header");
+const header = document.querySelector(".header");
 const headerBurger = document.querySelector(".header__burger");
 const headerNav = document.querySelector(".header__nav");
 
-// let once = true;
-// let second = true;
-// document.addEventListener("scroll", () => {
-//   if (window.pageYOffset > 110 && once) {
-//     once = false;
-//     second = true;
-//     header.style.backgroundColor = "white";
-//   }
-//   if (window.pageYOffset < 100 && second) {
+let once = true;
+let second = true;
+document.addEventListener("scroll", () => {
+  if (window.pageYOffset > 110 && once) {
+    once = false;
+    second = true;
+    header.style.backgroundColor = "white";
+  }
+  if (window.pageYOffset < 100 && second) {
 
-//     second = false;
-//     once = true;
-//     header.style.backgroundColor = "inherit";
-//   }
-// });
+    second = false;
+    once = true;
+    header.style.backgroundColor = "inherit";
+  }
+});
 
-// window.onbeforeunload = function () {
-//   window.scrollTo(0, 0);
-// };
+window.onbeforeunload = function () {
+  window.scrollTo(0, 0);
+};
 
 
 headerBurger.addEventListener("click", (event) => {
